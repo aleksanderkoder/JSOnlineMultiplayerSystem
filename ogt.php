46380532ff9907250911f781370e25c2b895cfb3
aleksanderkoder <49065176+aleksanderkoder@users.noreply.github.com>
2020-12-05 05:00:05 +0100

    Init

---
+++ ogt.php
@@ -0,0 +1,36 @@
+<?php
+
+$servername = "localhost";
+$username = "id15398232_ogt";
+$password = "Konbra7250**";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,"id15398232_onlinegametest");
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  echo json_encode("Can't connect to database");
+}
+
+$sql = "SELECT * FROM Player";
+$res = $conn->query($sql);
+
+//echo json_encode($conn->affected_rows);
+    if($conn->affected_rows > 0) 
+    {
+        $UserInfo = array();
+        while($row = $res->fetch_object())
+        {
+            $UserInfo[] = $row->ID;
+            $UserInfo[] = $row->PlayerName;
+            $UserInfo[] = $row->LeftPos;
+            $UserInfo[] = $row->TopPos;
+        }
+    
+        echo json_encode($UserInfo);
+        }
+        else
+        {
+            echo json_encode("Error from database");
+        }     
---
+++ ogtUpdatePlayerPos.php
@@ -0,0 +1,32 @@
+<?php
+
+$servername = "localhost";
+$username = "id15398232_ogt";
+$password = "Konbra7250**";
+$leftpos = $_POST["LeftPos"];
+$toppos = $_POST["TopPos"];
+$name = $_POST["Name"];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,"id15398232_onlinegametest");
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+  echo json_encode("Can't connect to database");
+}
+
+$sql = "UPDATE Player SET LeftPos = '$leftpos', TopPos = '$toppos' WHERE PlayerName = '$name'";
+$res = $conn->query($sql);
+
+
+    if($conn->affected_rows > 0) 
+    {
+        
+    
+        echo json_encode("OK");
+        }
+        else
+        {
+            echo json_encode("Error");
+        }  
